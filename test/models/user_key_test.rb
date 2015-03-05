e2d782cfd8145f1bfe9ef991fb37ee32f04947f0
Melanie Freeman <mdf@andrew.cmu.edu>
2015-03-04 22:01:47 -0500

    Added to user_key model, had to add matching tests to contexts and user_key tests

--- test/models/user_key_test.rb
+++ test/models/user_key_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class UserKeyTest < ActiveSupport::TestCase
+  # Relationships
+  should belong_to(:user)
+  should have_many(:user_key_organizations)
+  should have_many(:user_key_filters)
+  should have_many(:comments)
+  should have_many(:approvals)
+  
   # test "the truth" do
   #   assert true
   # end
+  context "Creating a user key context" do
+    setup do
+      create_everything
+    end
+    
+    teardown do
+      destroy_everything
+    end
+    
+    should "have a method that indicates that the key has been approved by all approvers" do
+      assert @bender_key.approved_by_all?
+    end
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,11 +5,19 @@ SimpleCov.start 'rails'
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+
+# Nice formatting of errors
 require "minitest/reporters"
 Minitest::Reporters.use!
 
+# Requiring contexts file
+require 'contexts'
+
 class ActiveSupport::TestCase
   ActiveRecord::Migration.check_pending!
+  
+  # Contexts for factory girl testing
+  include Contexts
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   # Commenting out fixtures

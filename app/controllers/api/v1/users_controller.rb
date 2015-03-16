adff9d2436b70a2d208b01dd7c3a7d50cd553ffb
Ben Lam <bklam@andrew.cmu.edu>
2015-03-16 11:03:47 -0400

    Added in basic API controller along with code to build basic JSON response

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,23 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+
+      def index
+        # basic code I had here to figure out how to build a response
+        # using Jbuilder. This code responds with all of the users in our
+        # shinymetal application, I will then comment this out and add in the
+        # other api script
+        @users = User.all
+        if !@users.nil?
+          @users = User.all
+          respond_to do |format|
+            format.json {render json: @users, status: 200}
+          end
+        else
+          render json: {message: 'Resource not found'}, status: 404
+        end
+      end
+
+    end
+  end
+end

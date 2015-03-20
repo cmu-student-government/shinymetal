83c5d069075cd67bf6735e3cab2df1092f187bc8
jkcorrea <jcorrea257@gmail.com>
2015-03-20 12:43:56 -0400

    Adding is_admin method for convenience

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ActiveRecord::Base
   has_many :user_keys
-  
+
   scope :alphabetical, -> { order(:andrew_id) }
   scope :approvers, -> { where(is_approver: true) }
   scope :search, ->(param) { where("andrew_id LIKE '%#{param.to_s.downcase}%'") }
-  
+
   ROLE_LIST = ["requester", "admin"]
-  
+
+  def is_admin?
+    self.role == "admin"
+  end
 end

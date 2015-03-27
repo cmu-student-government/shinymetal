dec8d51af6776d844ba29f397a7354b088c18b29
Melanie Freeman <mdf@andrew.cmu.edu>
2015-03-27 10:00:19 -0400

    Removed andrew_id from possible inputs in edit user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,6 +51,6 @@ class UsersController < ApplicationController
     
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:andrew_id, :role, :active)
+      params.require(:user).permit(:role, :active)
     end
 end

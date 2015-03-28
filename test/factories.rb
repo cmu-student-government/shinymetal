db50ff812b1d2a8cd268fd9a8acd23d012f0968f
Melanie Freeman <mdf@andrew.cmu.edu>
2015-03-28 12:02:33 -0400

    Added proposal_texts to factories, added expiration dates to user key context

--- test/factories.rb
+++ test/factories.rb
@@ -10,7 +10,17 @@ FactoryGirl.define do
    # factory blueprint for user keys
   factory :user_key do
     association :user
+    agree true
     name "Bender Key"
+    proposal_text_one "Test"
+    proposal_text_two "Test"
+    proposal_text_three "Test"
+    proposal_text_four "Test"
+    proposal_text_five "Test"
+    proposal_text_six "Test"
+    proposal_text_seven "Test"
+    # Eight is optional
+    proposal_text_eight nil
     status "awaiting_submission"
   end
 

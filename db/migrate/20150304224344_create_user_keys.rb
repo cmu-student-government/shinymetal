2e7956e1727e079c1cda2d5587424f94a72435b5
Melanie Freeman <mdf@andrew.cmu.edu>
2015-04-07 00:28:33 -0400

    Creating Answer model

--- db/migrate/20150304224344_create_user_keys.rb
+++ db/migrate/20150304224344_create_user_keys.rb
@@ -9,14 +9,6 @@ class CreateUserKeys < ActiveRecord::Migration
       t.date :time_expired
       t.boolean :active, default: true
       t.string :name
-      t.text :proposal_text_one
-      t.text :proposal_text_two
-      t.text :proposal_text_three
-      t.text :proposal_text_four
-      t.text :proposal_text_five
-      t.text :proposal_text_six
-      t.text :proposal_text_seven
-      t.text :proposal_text_eight
       t.boolean :agree, default: false
 
       t.timestamps null: false

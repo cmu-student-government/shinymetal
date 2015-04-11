b69af7717deefd2f4c880472fcbec50c8e199780
jkcorrea <jcorrea257@gmail.com>
2015-04-11 03:35:02 -0400

    Remove rails 4.2 config

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,6 @@ module Shinymetal
     config.assets.precompile += %w( vendor/modernizr )
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # config.active_record.raise_in_transactional_callbacks = true
   end
 end

11ed2e0580d4f14ba27550ad27855445f4e3bc6d
Melanie Freeman <mdf@andrew.cmu.edu>
2015-04-11 23:24:07 -0400

    Modified api controller

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -7,45 +7,34 @@ module Api
       skip_before_filter :verify_authenticity_token
       # apparently it's bad to pass in tokens in the URL directly,
       # it can be unsafe. will fix in future
-      before_filter :verify_access_with_api_key
+      #####before_filter :verify_access_with_api_key
 
       def index
         # FIXME need to modify to consider related filters and build 
         # appropriate query to hit the collegiate link api
-        require "./lib/bridgeapi_connection.rb"
-
-        # get which endpoint from the URL, probably not the most elegant
-        # split on the slashes
-        post_url_list = (request.original_url).split("/")
-        # get the last element, guaranteed to be non-empty because a request
-        # must have URL
-        endpoint = post_url_list[(post_url_list.length) - 1]
-
-        # modified the script to hit only the specified endpoint
-        # parse the JSON string from the collegiate link API into a hash
-        body = JSON.parse(hit_api_endpoint(endpoint))
-
-        # safe and non-nil because of verify_access_with_key 
-        andrew_id = request.headers["HTTP_ANDREW_ID"]
-        api_key   = request.headers["HTTP_API_KEY"]
-
-        # additional information from collegiatelink API forwarded to requester
-        # so the user can then build an iterator to collect all of the information
-        # from all pages
-        request_info_labels = ["pageNumber", "pageSize", "totalItems", "totalPages"]
-        request_info_hash   = Hash[*(request_info_labels.map{|l| [l, body[l]]}).flatten]
+        # require "./lib/bridgeapi_connection.rb"
+        
+        # Note to Ben, if you want to test "users" easily,
+        # comment out the before_filter, and uncomment the next line (which gets a key with at least 1 column for users):
+        @user_key = UserKey.select{|uk| uk.columns.restrict_to("users").size>0}.to_a.first
+        
+        endpoint = params[:endpoint]
+        response = EndpointResponse.new(endpoint)
+        if response.failed
+          render json: {"message" => "error, the requested resource does not exist"}
+          return
+        end
 
         # find the appropriate filter_columns for a given user key
-        user_key_array = UserKey.find_by_id(find_user_key_id_by_andrew_id(andrew_id)).to_a
-        filter_columns = (!user_key_array.nil? && user_key_array.length > 0) ? user_key_array[0].columns.map{|c| [c.resource, c.column_name] } : []
-        resource_idx, column_name_idx = 0, 1
-        final_columns = filter_columns.select{|fc| fc[resource_idx] == endpoint}.map{|fc| fc[column_name_idx]}
+        final_columns = @user_key.columns.restrict_to(endpoint).to_a
 
-        if final_columns.length == 0
-           render json: {"message" => "error, no columns whitelisted"}
+        if final_columns.empty?
+          render json: {"message" => "error, no columns permitted for this resource"}
         else
-          result_hash = {"results" => body["items"].map{|result| result.select{ |k, v| final_columns.include?(k) } } }
-          final_hash  = request_info_hash.merge(result_hash)
+          #result_hash = {"results" => body["items"].map{|result| result.select{ |k, v| final_columns.include?(k) } } }
+          response.restrict_to_columns(final_columns)
+          #final_hash  = request_info_hash.merge(result_hash)
+          final_hash = response.to_hash
           render json: JSON(final_hash), status: 200
         end
 
@@ -74,8 +63,16 @@ module Api
           # safe because we know the andrew_id is in the system, and must be
           # first because all andrew_ids are guaranteed to be unique
           @cur_user = User.search(andrew_id)[0] 
+          
           # need to check all active, non-expired keys associatd with the user
-          return @cur_user.user_keys.active.not_expired.map{|uk| uk.gen_api_key}.include?(api_key)
+          #return @cur_user.user_keys.active.not_expired.confirmed.map{|uk| uk.gen_api_key}.include?(api_key)
+          for key in @cur_user.user_keys.active.not_expired.confirmed
+            if key.gen_api_key == api_key
+              @user_key = key
+              return true
+            end
+          end
+          
         end
         return false
       end

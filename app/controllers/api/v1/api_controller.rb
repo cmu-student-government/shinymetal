88a4834090c323a257479bc28fe5737610bc73b5
Melanie Freeman <mdf@andrew.cmu.edu>
2015-04-12 18:37:44 -0400

    Made it so that an inactive user cant use the api functionality

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -12,8 +12,6 @@ module Api
       def index
         # FIXME need to modify to consider related filters and build 
         # appropriate query to hit the collegiate link api
-        # require "./lib/bridgeapi_connection.rb"
-        
         # Note to Ben, if you want to test "users" easily,
         # comment out the before_filter, and uncomment the next line (which gets a key with at least 1 column for users):
         #@user_key = UserKey.select{|uk| uk.columns.restrict_to("users").size>0}.to_a.first
@@ -40,39 +38,35 @@ module Api
 
       end
 
-      def find_user_key_id_by_andrew_id(andrew_id)
-        @cur_user         = User.search(andrew_id)[0]
-        @user_keys_to_ids = @cur_user.user_keys.active.not_expired.confirmed.map{|uk| {uk.gen_api_key => uk.id} }
-        # guaranteed to be non-nil because only called in index controller
-        # after verify_access_with_api_key
-        puts @user_keys_to_ids
-        api_key = request.headers["HTTP_API_KEY"]
-        # map to a list of ids where if the id matches, then return that id else 0
-        result = @user_keys_to_ids.map{|d| !d[api_key].nil? ? d[api_key] : 0}
-        # the id will then be added to all zeroes and can be returned
-        id = result.reduce(:+)
-        # in the case where no user_keys are found to match the user and api_key
-        final_id = id > 0 ? id : nil
-        return final_id
-      end
+      #def find_user_key_id_by_andrew_id(andrew_id)
+      #  @cur_user         = User.search(andrew_id)[0]
+      #  @user_keys_to_ids = @cur_user.user_keys.active.not_expired.confirmed.map{|uk| {uk.gen_api_key => uk.id} }
+      #  # guaranteed to be non-nil because only called in index controller
+      #  # after verify_access_with_api_key
+      #  puts @user_keys_to_ids
+      #  api_key = request.headers["HTTP_API_KEY"]
+      #  # map to a list of ids where if the id matches, then return that id else 0
+      #  result = @user_keys_to_ids.map{|d| !d[api_key].nil? ? d[api_key] : 0}
+      #  # the id will then be added to all zeroes and can be returned
+      #  id = result.reduce(:+)
+      #  # in the case where no user_keys are found to match the user and api_key
+      #  final_id = id > 0 ? id : nil
+      #  return final_id
+      #end
       
       private
       #return whether the passed in api_key exists in our system
       def key_matches?(api_key, andrew_id)
-        if User.all.map{|u| u.andrew_id}.include?(andrew_id)
-          # safe because we know the andrew_id is in the system, and must be
-          # first because all andrew_ids are guaranteed to be unique
-          @cur_user = User.search(andrew_id)[0] 
-          
-          # need to check all active, non-expired keys associatd with the user
-          #return @cur_user.user_keys.active.not_expired.confirmed.map{|uk| uk.gen_api_key}.include?(api_key)
+        @cur_user = User.find_by_andrew_id(andrew_id)
+        if @cur_user
+          # Safe because all andrew_ids are guaranteed to be unique
+          # Find the user key that belngs to the given API number
           for key in @cur_user.user_keys.active.not_expired.confirmed
             if key.gen_api_key == api_key
               @user_key = key
               return true
             end
           end
-          
         end
         return false
       end
@@ -84,7 +78,9 @@ module Api
         if (api_key.nil? || andrew_id.nil?)
           render json: {error: "Error, bad request"}, status: 400
         elsif !(key_matches?(api_key, andrew_id))
-          render json: {error: "Error, unauthorized"}, status: 401
+          render json: {error: "Error, unauthorized user or API key"}, status: 401
+        elsif !@cur_user.active
+          render json: {error: "Error, the account associated with this andrew ID has been suspended"}, status: 401
         end
       end
 

f3d6e04b23b993670188831d3e50c4f969ce55b2
Melanie Freeman <mdf@andrew.cmu.edu>
2015-04-13 09:46:45 -0400

    Inactive user cannot login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,22 +8,31 @@ class SessionsController < ApplicationController
       andrew_id = remote_user.split("@")[0].downcase
       user = User.find_by_andrew_id(andrew_id)
 
-      if !user
-        user = User.new
-        user.andrew_id = andrew_id
-        user.role = ["jkcorrea", "mdf", "aditisar", "bklam"].include?(andrew_id) ? "admin" : "requester"
-        user.save!
+      # Create a new user account for any CMU person not already in the system
+      user ||= new_user(andrew_id)
+      
+      # Do not allow inactive users to login 
+      if user.active
+        session[:user_id] = user.id
+        flash[:notice] = "Welcome, #{user.name}! You are now logged in."
+        redirect_to session[:return_to] || root_path
+      else
+        flash[:alert] = "You are not permitted to login because your account has been suspended."
+        redirect_to root_path
       end
-
-      # session[:idea_seed] = Time.now.to_i
-      session[:user_id] = user.id
-      flash[:notice] = "Welcome, #{user.name}! You are now logged in."
-      redirect_to session[:return_to] || root_path
     end
   end
 
   def logout
     session[:user_id] = nil
     redirect_to "https://webiso.andrew.cmu.edu/logout.cgi"
+  end
+  
+  private
+  def new_user(andrew_id)
+    user = User.new
+    user.andrew_id = andrew_id
+    user.role = ["jkcorrea", "mdf", "aditisar", "bklam"].include?(andrew_id) ? "admin" : "requester"
+    user.save!
   end
 end

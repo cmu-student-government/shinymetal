20d41b1e7dd2b162d2db166651c517a4b512fd2d
Aditi Sarkar <aditi0821@gmail.com>
2015-04-22 16:05:01 -0400

    changed crontab cap line

--- config/deploy.rb
+++ config/deploy.rb
@@ -61,4 +61,4 @@ end
 
 before "deploy:assets:precompile", "deploy:symlink_shared"
 before "deploy:assets:precompile", "deploy:symlink_php_endpoints"
-after "deploy:updat_code", "whenever:update_crontab"
+after "deploy:assets:precompile", "whenever:update_crontab"

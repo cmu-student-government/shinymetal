a884a6c470660aa4b26580d737a8e70205584d0a
Melanie Freeman <mdf@andrew.cmu.edu>
2015-04-22 22:16:13 -0400

    Didn't really comment user key orgs, not much to mention there

--- app/models/user_key_organization.rb
+++ app/models/user_key_organization.rb
@@ -1,10 +1,12 @@
 class UserKeyOrganization < ActiveRecord::Base
+  # Relationships
+  
   belongs_to :organization
   belongs_to :user_key
+  
+  # Validations
 
-  # Require org id and user_key id to be in system
   validates_presence_of :user_key
   validates_presence_of :organization
-  
   validates_uniqueness_of :user_key_id, scope: :organization_id
 end

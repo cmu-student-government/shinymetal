11f836bc5f27cee443d87857cac8c945ba0071ff
Jake Correa <jcorrea257@gmail.com>
2015-12-10 12:45:13 -0500

    Add rvmrc to force 2.1.6

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,4 +2,4 @@ server 'stugov.andrew.cmu.edu', user: 'jkcorrea', roles: %w{web app}
 set :deploy_to, "/var/www/staging/webapps/#{fetch(:application)}"
 set :stage, :staging
 set :rails_env, :staging
-set :branch, :dev
+set :branch, 'dev'
